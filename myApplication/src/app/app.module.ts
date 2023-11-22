f554bfa27bfe7d342540fb9eab5f922d5a6befc6
sPratikshas <spratiksha208@gmail.com>
2023-11-22 17:56:20 -0500

    22 Nov Simple Component Routing

--- myApplication/src/app/app.module.ts
+++ myApplication/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [   //component/pipe/directive
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
   ],
-  imports: [
+  imports: [      //modules
     BrowserModule,
     AppRoutingModule
   ],
